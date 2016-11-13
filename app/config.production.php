ee8032f99aff2fedf924eb68479dd6ba230c3229
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2016-11-13 16:53:15 +0200

    Add live server credentials

---
+++ app/config.production.php
@@ -0,0 +1,14 @@
+<?php
+/* Author : Tsotets Thapelo
+ * Contact : me@tsotetesithapelo.co.za
+ * Copyright : GNU GPL*/
+
+    return array(
+        'hostname' => 'www.myres.co.za',
+        'port' => '5432',
+        'db_username' => 'myres',
+        'db_password' => '57dse0IfU2@nd_=enx2(ndf{J%!3',
+        'db_name' => 'db_myres_lbg_2017_live',
+        'db_type' => 'postgresql',
+        );
+?>
