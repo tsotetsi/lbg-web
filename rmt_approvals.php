159f0b3c49c75263b08943af3775c11fcbea8616
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2016-12-07 06:06:05 +0200

    Remove active header link.

--- rmt_approvals.php
+++ rmt_approvals.php
@@ -24,7 +24,7 @@
 		    </div>
 		    <div class="collapse navbar-collapse" id="myNavbar">
 		      <ul class="nav navbar-nav">
-		        <li><a href="/">Home</a></li>
+		        <li><a href="#">Home</a></li>
 		      </ul>
 		      <ul class="nav navbar-nav navbar-right">
 		        <li><a href="login.php"><span class="glyphicon glyphicon-log-out"></span> Login</a></li>
