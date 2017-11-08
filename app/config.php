3748bb6e8bd612112e2e8879447670012d24009c
Tsotetsi Thapelo <info@tsotetsithapelo.co.za>
2017-11-08 21:21:09 +0200

    fix dev settings :)

--- app/config.php
+++ app/config.php
@@ -3,12 +3,12 @@
  * Contact : me@tsotetesithapelo.co.za
  * Copyright : GNU GPL*/
 
-	return array(
-		'hostname' => '154.127.59.170',
-		'port' => '5432',
-		'db_username' => 'myres',
-		'db_password' => '57dse0IfU2@nd_=enx2(ndf{J%!3',
-		'db_name' => 'db_myres_lbg_2017_live',
-		'db_type' => 'postgresql',
-		);
+        return array(
+                'hostname' => 'localhost',
+                'port' => '5432',
+                'db_username' => 'webdev',
+                'db_password' => 'webdev',
+                'db_name' => 'lbgsample',
+                'db_type' => 'postgresql',
+                );
 ?>
